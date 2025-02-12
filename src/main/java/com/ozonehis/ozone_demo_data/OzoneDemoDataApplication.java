c704ae292f624c1e68881985ad4658fa233cb738
corneliouzbett <kipchubett@gmail.com>
2025-02-12 20:37:38 +0300

    OZ-439: Standardize code formatting + license headers

--- src/main/java/com/ozonehis/ozone_demo_data/OzoneDemoDataApplication.java
+++ src/main/java/com/ozonehis/ozone_demo_data/OzoneDemoDataApplication.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2025, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.ozonehis.ozone_demo_data;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +13,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OzoneDemoDataApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OzoneDemoDataApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(OzoneDemoDataApplication.class, args);
+    }
 }
--- src/test/java/com/ozonehis/ozone_demo_data/OzoneDemoDataApplicationTests.java
+++ src/test/java/com/ozonehis/ozone_demo_data/OzoneDemoDataApplicationTests.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2025, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.ozonehis.ozone_demo_data;
 
 import org.junit.jupiter.api.Test;
@@ -6,8 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OzoneDemoDataApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {}
 }
