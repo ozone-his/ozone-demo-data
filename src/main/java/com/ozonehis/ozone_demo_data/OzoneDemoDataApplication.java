5f1943d3288bbdfeb4f275fd1a84457aa72604c6
corneliouzbett <kipchubett@gmail.com>
2025-02-12 19:13:43 +0300

    OZ-439: Initialize Ozone Demo Data app with Spring Boot setup

---
+++ src/main/java/com/ozonehis/ozone_demo_data/OzoneDemoDataApplication.java
@@ -0,0 +1,13 @@
+package com.ozonehis.ozone_demo_data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OzoneDemoDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OzoneDemoDataApplication.class, args);
+	}
+
+}
