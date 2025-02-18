e44878bfbc36596ae6d8222f292fc38c31fe5d4d
corneliouzbett <kipchubett@gmail.com>
2025-02-18 18:21:32 +0300

    OZ-349: Add RestTemplate Spring bean

---
+++ src/main/java/com/ozonehis/ozone_demo_data/config/BeanConfiguration.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright © 2025, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.ozone_demo_data.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class BeanConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
