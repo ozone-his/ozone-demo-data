e923c742121faa5d7e958fed4c8a66d345fc15b1
corneliouzbett <kipchubett@gmail.com>
2025-02-21 18:32:54 +0300

    OZ-349: Implement DemoDataGeneration and KeycloakUserCreation tasks

---
+++ src/main/java/com/ozonehis/ozone_demo_data/task/TaskExecutor.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2025, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.ozone_demo_data.task;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * This interface defines the contract for executing tasks.
+ */
+public interface TaskExecutor {
+
+    /**
+     * Executes the task.
+     *
+     * @param latch the countdown latch to signal when the task is complete
+     */
+    void executeAsync(CountDownLatch latch);
+}
