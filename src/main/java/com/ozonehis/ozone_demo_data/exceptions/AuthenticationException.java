28c5b4b9a67f57be02355241aacd869476168877
corneliouzbett <kipchubett@gmail.com>
2025-03-03 09:29:57 +0300

    OZ-349: Custom exceptions for authentication and demo data generation

---
+++ src/main/java/com/ozonehis/ozone_demo_data/exceptions/AuthenticationException.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright © 2025, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package com.ozonehis.ozone_demo_data.exceptions;
+
+public class AuthenticationException extends RuntimeException {
+
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
